7ffc0be6f6a8483bbc96820e3473ecbfa3512179
Fabian Vogelsteller <fabian@frozeman.de>
2015-07-14 18:11:49 +0200

    fixed package.js

--- package.js
+++ package.js
@@ -12,9 +12,9 @@ Package.onUse(function(api) {
   api.use('tracker', 'client');
 
   api.use('frozeman:persistent-minimongo@0.1.3', 'client');
-  api.use('ethereum:web3@0.7.0', 'client');
+  api.use('ethereum:web3@0.8.1', 'client');
 
-  api.export(['lastBlock', 'Blocks', 'web3'], 'client'); // we need to expose web3.js, so that the app, can re-use this one, instead of having two instances
+  api.export(['Blocks', 'web3'], 'client'); // we need to expose web3.js, so that the app, can re-use this one, instead of having two instances
 
   api.addFiles('blocks.js', 'client');
 });
