f88e4ac466d612aed151ac6984c53c3b9e15a626
Fabian Vogelsteller <fabian@frozeman.de>
2015-09-03 11:47:30 +0200

    dont expose web3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:blocks',
   summary: 'Provides informations about the current and last 50 blocks',
-  version: '0.1.8',
+  version: '0.1.9',
   git: 'http://github.com/ethereum/meteor-package-blocks'
 });
 
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.use('mongo', ['client', 'server']);
 
   api.use('frozeman:persistent-minimongo@0.1.3', 'client');
-  api.use('ethereum:web3@0.8.1', ['client', 'server']);
+  api.use('ethereum:web3@0.12.2', ['client', 'server']);
 
-  api.export(['EthBlocks', 'web3'], ['client', 'server']); // we need to expose web3.js, so that the app, can re-use this one, instead of having two instances
+  api.export(['EthBlocks'], ['client', 'server']);
 
   api.addFiles('blocks.js', ['client', 'server']);
 });
